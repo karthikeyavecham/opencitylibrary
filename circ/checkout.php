a7b7e72a775e1c0258d970655295e2d2ea52def9
Karthikeya Vecham <karthikeya.vecham@gmail.com>
2013-08-19 19:26:14 +0530

    Enhancement #9 working
    Enhancement #9 will work now. small change has been made to work.

--- circ/checkout.php
+++ circ/checkout.php
@@ -51,13 +51,14 @@
     $err = $circQ->checkout_due_e($mbr->getBarcodeNmbr(), $barcode, $dueDate);
     checkerror('barcodeNmbr', $err);
   } else {
+  	$user=$_SESSION['userid'];
 	$err=$circQ->validateCheckOut($user,$barcode);
 	if($err==NULL)
 	{
 	    $err = $circQ->checkout_e($mbr->getBarcodeNmbr(), $barcode);		
 	}
 	checkerror('barcodeNmbr', $err);
-	$user=$_SESSION['userid'];
+
   }
 
   unset($_SESSION["postVars"]);
